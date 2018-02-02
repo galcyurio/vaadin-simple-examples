fa964773474f32bbab2e7ade2b0cd9d40016d915
galcyurio <galcyurio@gmail.com>
2018-02-02 12:30:38 +0900

    [WIP] drag-and-drop project added

---
+++ tut-vaadin-simple-drag-and-drop/src/main/kotlin/com/github/galcyurio/ui/MainUI.kt
@@ -0,0 +1,15 @@
+package com.github.galcyurio.ui
+
+import com.vaadin.server.VaadinRequest
+import com.vaadin.spring.annotation.SpringUI
+import com.vaadin.ui.UI
+
+/**
+ * @author galcyurio
+ */
+@SpringUI
+class MainUI : UI() {
+    override fun init(p0: VaadinRequest?) {
+
+    }
+}
