5f4302cbae259453c86f565a28f2f814b1de513a
galcyurio <galcyurio@gmail.com>
2018-02-02 16:14:09 +0900

    sidebar badge updated

--- tut-vaadin-simple-sidebar/src/main/kotlin/com/github/galcyurio/ui/MainUI.kt
+++ tut-vaadin-simple-sidebar/src/main/kotlin/com/github/galcyurio/ui/MainUI.kt
@@ -5,9 +5,10 @@ import com.vaadin.annotations.Theme
 import com.vaadin.server.Responsive
 import com.vaadin.server.VaadinRequest
 import com.vaadin.spring.annotation.SpringUI
-import com.vaadin.ui.CssLayout
 import com.vaadin.ui.HorizontalLayout
+import com.vaadin.ui.TextField
 import com.vaadin.ui.UI
+import com.vaadin.ui.VerticalLayout
 import com.vaadin.ui.themes.ValoTheme
 
 /**
@@ -22,7 +23,16 @@ class MainUI : UI() {
         addStyleName(ValoTheme.UI_WITH_MENU)
 
         content = HorizontalLayout().apply {
-            addComponents(SideBar("Vaadin Menu"), CssLayout())
+            val sideBar = SideBar("Vaadin Menu")
+
+            addComponents(
+                sideBar,
+                VerticalLayout().apply {
+                    addComponent(TextField("badge text", { event ->
+                        sideBar.setBadge(event.value)
+                    }))
+                }
+            )
             setSizeFull()
         }
     }
